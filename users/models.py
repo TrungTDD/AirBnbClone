2eebbf4b09020e8efba4b689ef678814652fb2be
trungtruong <duytrungdng@gmail.com>
2024-02-22 21:45:24 -0500

    add user models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    MALE_GENDER = "M"
+    FEMALE_GENDER = "F"
+    GENDER = [
+        (MALE_GENDER, "Male"),
+        (FEMALE_GENDER, "Female"),
+    ]
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_VIETNAMESE = "vn"
+    LANGUAGE = [(LANGUAGE_ENGLISH, "English"), (LANGUAGE_VIETNAMESE, "Vietnamese")]
+
+    CURRENCY_USD = "USD"
+    CURRENCY_VND = "VND"
+    CURRENCY = [(CURRENCY_USD, "USD"), (CURRENCY_VND, "VND")]
+
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(max_length=255, choices=GENDER, default=MALE_GENDER)
+    bio = models.TextField(default="")
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        max_length=255, choices=LANGUAGE, default=LANGUAGE_ENGLISH
+    )
+    currency = models.CharField(max_length=255, choices=CURRENCY, default=CURRENCY_USD)
+    superhost = models.BooleanField(default=False)
